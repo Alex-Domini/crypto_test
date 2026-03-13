d44a1f248f958cb34c5c81db36c963a63cf64bd1
Alex-Domini <102829194+Alex-Domini@users.noreply.github.com>
2026-03-13 22:08:07 +0700

    feat: add database config, session and price model

---
+++ app/core/config.py
@@ -0,0 +1,43 @@
+import urllib.parse
+from pydantic import computed_field, Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str = Field(default=...)
+    POSTGRES_PASSWORD: str = Field(default=...)
+    POSTGRES_HOST: str = "localhost"
+    POSTGRES_PORT: int = 5432
+    POSTGRES_DB: str = Field(default=...)
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+    @computed_field
+    @property
+    def DATABASE_URL_ASYNC(self) -> str:
+        # Экранируем пароль на случай спецсимволов
+        password = urllib.parse.quote_plus(self.POSTGRES_PASSWORD)
+        return (
+            f"postgresql+asyncpg://"
+            f"{self.POSTGRES_USER}:@"
+            f"{password}@"
+            f"{self.POSTGRES_HOST}:"
+            f"{self.POSTGRES_PORT}/"
+            f"{self.POSTGRES_DB}"
+        )
+
+    @computed_field
+    @property
+    def DATABASE_URL_SYNC(self) -> str:
+        password = urllib.parse.quote_plus(self.POSTGRES_PASSWORD)
+        return (
+            f"postgresql://"
+            f"{self.POSTGRES_USER}:"
+            f"{password}@"
+            f"{self.POSTGRES_HOST}:"
+            f"{self.POSTGRES_PORT}/"
+            f"{self.POSTGRES_DB}"
+        )
+
+
+settings = Settings()
